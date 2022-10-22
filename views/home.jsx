9d2236f2b5d8fabc113944901d5a8a69a7455494
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-22 11:30:11 -0400

    Adding styles to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -3,16 +3,20 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-          <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/fries-is-served.jpg" alt="Fries" />
+                <div> Photo by <a href="AUTHOR_LINK">Lucas Andrade</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
