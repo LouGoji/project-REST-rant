0cc3abadbc3b2bb633b62716e3083ec1a5a666eb
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-24 21:26:48 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -21,7 +21,35 @@ router.delete('/:id', (req, res) => {
   }
   else {
     places.splice(id, 1)
-    res.redirect('/places')
+    res.redirect('places/show')
+  }
+})
+
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id})
   }
 })
 
@@ -53,24 +81,6 @@ router.put('/:id', (req, res) => {
   }
 })
 
-
-
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
 
 
   
